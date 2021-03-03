07fa7f9279676ee3082a6da6947f292ee292ada4
Adriano F. Rosso <rosso.af@gmail.com>
2021-03-02 22:01:49 -0300

    add duplicity verification

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, json
+from flask import Flask, render_template, request, redirect, url_for
+import json
+import os.path
 
 app = Flask(__name__)
 
@@ -10,6 +12,17 @@ def home():
 def your_url():
   if(request.method == 'POST'):
     urls = {}
+
+    # First we need to check if the url already exists
+    # in our file
+    if os.path.exists('urls.json'):
+      with open('urls.json') as urls_file:
+        urls = json.load(urls_file)
+
+      if request.form['code'] in urls.keys():
+        return redirect(url_for('home'))
+
+    # If the url does not exist, we create it in our file
     urls[request.form['code']] = {'url': request.form['url']}
     with open('urls.json', 'w') as url_file:
       json.dump(urls, url_file)
