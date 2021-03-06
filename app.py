3fb3bd48d93190c318f1b1b22be4e3edf7ae9e94
Adriano F. Rosso <rosso.af@gmail.com>
2021-03-06 15:09:49 -0300

    add session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, abort
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -13,7 +13,7 @@ FILE_PATH = os.getenv('FILE_PATH')
 
 @app.route('/')
 def home():
-  return render_template('home.html')
+  return render_template('home.html', codes=session.keys())
 
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
@@ -26,11 +26,12 @@ def your_url():
       with open('urls.json') as urls_file:
         urls = json.load(urls_file)
 
-      # If the url does not exist, we create it in our file
+      # If the url EXISTS, we warn the user
       if request.form['code'] in urls.keys():
         flash('This key is already taken. Please chose another one.')
         return redirect(url_for('home'))
 
+      # if the key DOES NOT EXIST, we create it in our file
       if 'url' in request.form.keys():
         urls[request.form['code']] = {'url': request.form['url']}
       else:
@@ -41,6 +42,7 @@ def your_url():
 
     with open('urls.json', 'w') as url_file:
       json.dump(urls, url_file)
+      session[request.form['code']] = True
     return render_template('your-url.html', code=request.form['code'])
   else:
     return redirect(url_for('home'))
